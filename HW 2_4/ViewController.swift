ba112889af25f0ff1bf24b8989932787ea16867e
Anna Ablogina <yunann95@gmail.com>
2024-02-02 10:21:30 +0300

    stupid version

--- HW 2_4/ViewController.swift
+++ HW 2_4/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorValuesLabels: [UILabel]!
+    @IBOutlet var colorView: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupUI()
     }
 
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        let label = colorValuesLabels[sender.tag]
+        label.text = String(format: "%.2f", sender.value)
+        
+        var colors: [CGFloat] = []
+        colorValuesLabels.forEach { label in
+            colors.append(CGFloat(Float(label.text ?? "0") ?? 0))
+        }
+        
+        colorView.backgroundColor = UIColor(
+            red: colors[0],
+            green: colors[1],
+            blue: colors[2],
+            alpha: 1
+        )
+    }
+    
+    private func setupUI() {
+        colorView.layer.cornerRadius = 20
 
+    }
+    
 }
 
